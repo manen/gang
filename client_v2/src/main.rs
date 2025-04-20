353ab7479762075445a74aab03256926c2ae5e77
manen <49951816+manen@users.noreply.github.com>
2025-04-20 18:41:02 +0200

    `client_v2`: task-based bots basically the swarm has a long list of tasks queued up, if none are queued then the task is to follow manen_ if there are tasks in the queue, each bot will take the one at the front and execute it

---
+++ client_v2/src/main.rs
@@ -0,0 +1,143 @@
+#![feature(duration_constructors)]
+
+use std::{sync::Arc, time::Duration};
+
+use azalea::{
+	Account, Client, Event,
+	swarm::{Swarm, SwarmBuilder, SwarmEvent},
+};
+use tasks::{Task, Tasks};
+use tokio::{sync::Mutex, task::JoinHandle};
+
+pub mod tasks;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+	println!("Hello, world!");
+
+	std::thread::spawn(move || {
+		loop {
+			std::thread::sleep(Duration::from_secs(10));
+			let deadlocks = parking_lot::deadlock::check_deadlock();
+			if deadlocks.is_empty() {
+				continue;
+			}
+
+			println!("{} deadlocks detected", deadlocks.len());
+			for (i, threads) in deadlocks.iter().enumerate() {
+				println!("Deadlock #{}", i);
+				for t in threads {
+					println!("Thread Id {:#?}", t.thread_id());
+					println!("{:#?}", t.backtrace());
+				}
+			}
+		}
+	});
+
+	let accounts = |suffix| {
+		["pop", "bob", "test", "bot", "stick"]
+			.into_iter()
+			.map(move |base| format!("{base}{suffix}"))
+	};
+	let accounts = accounts("")
+		.chain(accounts("_1"))
+		// .chain(accounts("_2"))
+		// .chain(accounts("_3"))
+		.map(|name| Account::offline(name.as_ref()))
+		.collect::<Vec<_>>();
+
+	let mut builder = SwarmBuilder::new()
+		.set_handler(handle)
+		.set_swarm_handler(swarm_handler);
+
+	// tasks are created here, execution starts on Event::Spawn
+
+	let tasks = Tasks::default();
+
+	for account in accounts {
+		builder = builder.add_account_with_state(
+			account,
+			State {
+				tasks: tasks.clone(),
+				handle: Arc::new(Mutex::new(None)),
+			},
+		)
+	}
+
+	builder
+		.set_swarm_state(State {
+			tasks: tasks.clone(),
+			handle: Arc::new(Mutex::new(None)),
+		})
+		.join_delay(Duration::from_millis(50))
+		.start("localhost")
+		.await?;
+}
+
+#[derive(Default, Clone, bevy_ecs_macros::Component, bevy_ecs_macros::Resource)]
+pub struct State {
+	tasks: Tasks,
+	handle: Arc<Mutex<Option<JoinHandle<()>>>>,
+}
+
+async fn swarm_handler(swarm: Swarm, event: SwarmEvent, state: State) {
+	match event {
+		SwarmEvent::Chat(m) => {
+			let content = m.content();
+			let mut words = content.split(' ');
+
+			if words.next() == Some("gang") {
+				match state.tasks.handle_command(words).await {
+					Ok(a) => a,
+					Err(err) => eprintln!("couldn't parse command {}: {err}", m.content()),
+				};
+			}
+		}
+		_ => {}
+	}
+}
+
+async fn handle(bot: Client, event: Event, state: State) -> anyhow::Result<()> {
+	match event {
+		Event::Chat(m) => {
+			let content = m.content();
+			let mut words = content.split(' ');
+
+			let username = bot.username();
+			let name = words.next();
+			if name == Some("gang") || name == Some(&username) {
+				if words.next() == Some("say") {
+					let rest = utils::Join::new(words, std::iter::repeat(" ")).collect::<String>();
+					bot.chat(&rest);
+				}
+			}
+		}
+		Event::Spawn => {
+			state.tasks.tick(&bot).await;
+			if state.handle.lock().await.is_none() {
+				let mut handle = state.handle.lock().await;
+				*handle = Some(tokio::spawn(async move {
+					loop {
+						let next = state.tasks.next().await;
+						if next == Some(Task::Halt) {
+							break;
+						}
+						if let Some(task) = next {
+							match task.execute(&bot).await {
+								Ok(a) => a,
+								Err(err) => {
+									eprintln!("{} couldn't execute {task:?}: {err}", bot.username())
+								}
+							};
+						}
+					}
+				}));
+			}
+		}
+		Event::Tick => {
+			state.tasks.tick(&bot).await;
+		}
+		_ => {}
+	}
+	Ok(())
+}
---
+++ client_v2/src/tasks.rs
@@ -0,0 +1,199 @@
+use std::{
+	collections::VecDeque,
+	sync::Arc,
+	time::{Duration, Instant},
+};
+
+use anyhow::Context;
+use anyhow::anyhow;
+use azalea::{
+	BlockPos, BotClientExt, Client, GameProfileComponent, Vec3,
+	entity::{Position, metadata::Player},
+	pathfinder::goals::{Goal, RadiusGoal},
+	prelude::PathfinderClientExt,
+	swarm::Swarm,
+};
+use bevy_ecs::query::With;
+use tokio::sync::Mutex;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Task {
+	Halt,
+	Jump,
+	Goto(RadiusGoal),
+	Mine(BlockPos),
+}
+impl Task {
+	pub async fn execute(&self, bot: &Client) -> anyhow::Result<()> {
+		match self {
+			Self::Jump => {
+				bot.jump();
+			}
+			Self::Goto(goal) => {
+				if !goal.success(bot.position().to_block_pos_floor()) {
+					bot.start_goto(*goal);
+					tokio::time::sleep(Duration::from_millis(500)).await
+				} else {
+					tokio::time::sleep(Duration::from_millis(500)).await
+				}
+			}
+			Self::Mine(pos) => {
+				if !bot
+					.world()
+					.read()
+					.get_block_state(pos)
+					.map(|state| state.is_air())
+					.unwrap_or(false)
+				{
+					let goal = RadiusGoal {
+						pos: pos.center(),
+						radius: 3.5,
+					};
+
+					if !goal.success(bot.position().to_block_pos_floor()) {
+						bot.goto(goal).await;
+					}
+					bot.look_at(pos.center());
+					bot.mine(*pos).await;
+
+					tokio::time::sleep(Duration::from_millis(50)).await;
+				}
+			}
+			Self::Halt => {}
+		}
+		Ok(())
+	}
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct OwnerPos {
+	time: Instant,
+	pos: Vec3,
+}
+impl Default for OwnerPos {
+	fn default() -> Self {
+		Self {
+			time: Instant::now() - Duration::new(60 * 60 * 10, 0),
+			pos: Default::default(),
+		}
+	}
+}
+impl OwnerPos {
+	pub fn new(pos: Vec3) -> Self {
+		Self {
+			time: Instant::now(),
+			pos,
+		}
+	}
+}
+
+#[derive(Default, Clone, Debug)]
+pub struct Tasks {
+	owner_pos: Arc<Mutex<OwnerPos>>,
+	queue: Arc<Mutex<VecDeque<Task>>>,
+}
+impl Tasks {
+	pub async fn next(&self) -> Option<Task> {
+		let from_queue = {
+			let mut queue = self.queue.lock().await;
+			queue.pop_front()
+		};
+		if let Some(from_queue) = from_queue {
+			Some(from_queue)
+		} else {
+			let owner_pos = self.owner_pos.lock().await.clone();
+			if owner_pos.time.elapsed() < Duration::from_mins(2) {
+				Some(Task::Goto(RadiusGoal {
+					pos: owner_pos.pos,
+					radius: 10.0,
+				}))
+			} else {
+				Some(Task::Jump)
+			}
+		}
+	}
+
+	pub async fn tick(&self, bot: &Client) {
+		let age = {
+			let owner_pos = self.owner_pos.lock().await;
+			owner_pos.time.elapsed()
+		};
+
+		if age > Duration::from_millis(300) {
+			let entity = bot.entity_by::<With<Player>, &GameProfileComponent>(
+				|profile: &&GameProfileComponent| profile.name == "manen_",
+			);
+			if let Some(player) = entity {
+				let pos: Option<Position> = bot.get_entity_component(player);
+				if let Some(pos) = pos {
+					let mut owner_pos = self.owner_pos.lock().await;
+					*owner_pos = OwnerPos::new(pos.down(0.0));
+				}
+			}
+		}
+	}
+
+	pub async fn handle_command<'a, I: IntoIterator<Item = &'a str>>(
+		&self,
+		words: I,
+	) -> anyhow::Result<()> {
+		let mut words = words.into_iter();
+		match words.next() {
+			Some("demolish") => {
+				let from_x: i32 = words
+					.next()
+					.ok_or_else(|| anyhow!("expected x coordinate"))?
+					.parse()?;
+				let from_y: i32 = words
+					.next()
+					.ok_or_else(|| anyhow!("expected y coordinate"))?
+					.parse()?;
+				let from_z: i32 = words
+					.next()
+					.ok_or_else(|| anyhow!("expected z coordinate"))?
+					.parse()?;
+
+				let to_x: i32 = words
+					.next()
+					.ok_or_else(|| anyhow!("expected x coordinate"))?
+					.parse()?;
+				let to_y: i32 = words
+					.next()
+					.ok_or_else(|| anyhow!("expected y coordinate"))?
+					.parse()?;
+				let to_z: i32 = words
+					.next()
+					.ok_or_else(|| anyhow!("expected z coordinate"))?
+					.parse()?;
+
+				let from = (from_x, from_y, from_z);
+				let to = (to_x, to_y, to_z);
+
+				let from_x = from.0.max(to.0);
+				let from_y = from.1.max(to.1);
+				let from_z = from.2.max(to.2);
+				let to_x = from.0.min(to.0);
+				let to_y = from.1.min(to.1);
+				let to_z = from.2.min(to.2);
+
+				{
+					let mut new_queue = VecDeque::new();
+					for y in (to_y..from_y + 1).rev() {
+						for x in to_x..from_x + 1 {
+							for z in to_z..from_z + 1 {
+								new_queue.push_back(Task::Mine(BlockPos { x, y, z }));
+							}
+						}
+					}
+					{
+						let mut queue = self.queue.lock().await;
+						let taken_queue = std::mem::replace(&mut *queue, Default::default());
+						*queue = taken_queue.into_iter().chain(new_queue).collect();
+					}
+				}
+			}
+			_ => {}
+		}
+		Ok(())
+	}
+}
